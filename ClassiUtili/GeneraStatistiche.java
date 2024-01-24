822f679c8d62ff458438afc21b765f1b0736ed30
raff-imperato <115695791+raff-imperato@users.noreply.github.com>
2024-01-24 01:30:39 +0100

    Add files via upload

--- ClassiUtili/GeneraStatistiche.java
+++ ClassiUtili/GeneraStatistiche.java
@@ -5,29 +5,29 @@ public class GeneraStatistiche {
 
     public static void printIC(ArrayList<DataTest> dataTests, String classUT) {
         DataTest.sortByInstructionCoverage(dataTests);
-        System.out.printf("%-20s %-20s %-20s%n", "Categoria", "NomeTester", "InstructionCoverage");
+        System.out.printf("%-20s %-20s %-20s%n", "Categoria", "|", "NomeTester", "|", "InstructionCoverage");
         for(DataTest d : dataTests) {
             if(d.getClassUT().equals(classUT)) {
-                System.out.printf("%-20s %-20s %-20.2f%n", d.getCategoria() , " " + d.getNomeTester() , " " , d.getCoverageJaCoCo().getInstructionCoverage());
+                System.out.printf("%-20s %-20s %-20.2f%n", d.getCategoria() , "|" , d.getNomeTester() , "|" , d.getCoverageJaCoCo().getInstructionCoverage());
             }
         }
     }
 
     public static void printWM(ArrayList<DataTest> dataTests, String classUT) {
         DataTest.sortByWeakMutation(dataTests);
-        System.out.printf("%-20s %-20s %-20s%n", "Categoria", "NomeTester", "WeakMutation");
+        System.out.printf("%-20s %-20s %-20s%n", "Categoria", "|", "NomeTester", "|", "WeakMutation");
         for(DataTest d : dataTests) {
             if(d.getClassUT().equals(classUT)) {
-                System.out.printf("%-20s %-20s %-20.2f%n", d.getCategoria() , " " + d.getNomeTester() , " " , d.getCoverageEvoSuite().getWeakMutation());
+                System.out.printf("%-20s %-20s %-20.2f%n", d.getCategoria() , "|" , d.getNomeTester() , "|" , d.getCoverageEvoSuite().getWeakMutation());
             }
         }
     }
 
     public static void printLC(ArrayList<DataTest> dataTests, String classUT) {
-        System.out.printf("%-20s %-20s %-20s %-20s%n", "Categoria", "NomeTester", "LineCoverageJaCoCo", "LineCoverageEvoSuite");
+        System.out.printf("%-20s %-20s %-20s %-20s%n", "Categoria", "|", "NomeTester", "|", "LineCoverageJaCoCo", "|", "LineCoverageEvoSuite");
         for(DataTest d : dataTests) {
             if(d.getClassUT().equals(classUT)) {
-                System.out.printf("%-20s %-20s %-20.2f %-20.2f%n", d.getCategoria(), d.getNomeTester(), d.getCoverageJaCoCo().getLineCoverage(), d.getCoverageEvoSuite().getLineCoverage());
+                System.out.printf("%-20s %-20s %-20.2f %-20.2f%n", d.getCategoria(), "|" , d.getNomeTester(), "|", d.getCoverageJaCoCo().getLineCoverage(), "|", d.getCoverageEvoSuite().getLineCoverage());
             }
         }
     }
